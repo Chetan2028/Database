f2fcccfda3d7106c068352916d2aea0b1fdeac14
Chetan2028 <bmchetan2028@gmail.com>
2020-11-23 07:46:03 +0530

    [Chetan] Refactor . refactored table structure and performed queries

--- Employee_Payroll.sql
+++ Employee_Payroll.sql
@@ -72,3 +72,114 @@ Alter table Employee_Payroll add Basic_Pay money not null default 0 , Deductions
 --UC10 --> To make Employee Chetan as part of Cloud and Developing
 update Employee_Payroll set Emp_Name = 'Chetan' , Gender = 'M' where Emp_Name = 'Apoorva';
 
+
+--UC11--
+/*-----------------------------CREATE EMPLOYEE TABLE--------------------------*/
+create table Employee
+(
+Employee_Id int identity(1,1) not null,
+Emp_Name varchar(25) not null,
+Start_Date date not null,
+Gender varchar(10) not null,
+Emp_Address varchar(100) not null,
+Dept_no int not null,
+Phone_Number varchar(10) not null,
+PRIMARY KEY (Employee_Id),
+FOREIGN KEY (Dept_no) REFERENCES Department(Dept_no)
+);
+
+
+/*----------------------TO INSERT THE VALUES----------------------*/
+insert into Employee values
+('Bill','2018-01-03','M', 'Brooklyn', 1,'692587412'),
+('Terissa','2019-11-13','F', 'Manhattan', 2,'1236547890'),
+('Charlie','2020-05-21','M', 'New Jersey', 3,'8523697410');
+
+/*------------------------TO RETIREVE THE DATA-------------------------------*/
+Select * from Employee
+
+/*------------------------------CREATE DEPARTMENT TABLE---------------------------*/
+create table Department
+(
+Dept_no int  not null,
+Dept_name varchar(30) not null,
+Dept_location varchar(50) not null,
+PRIMARY KEY (Dept_no)
+);
+
+
+/*---------------------------RETIREVE THE DATA------------------------*/
+select * from Department
+
+/*---------------------------INSERT THE VALUES----------------------*/
+insert into Department values
+(1,'Delivery','Manhattan'),
+(2,'Sales','Boston'),
+(3,'HR','Illinois');
+
+/*---------------------CREATE SALARY TABLE----------------------*/
+create table Salary(
+Salary_id int identity(1,1) not null,
+Employee_Id int not null,
+Basic_pay money not null,
+Deductions money not null,
+Taxable_pay money not null,
+Income_tax money not null,
+Net_pay money not null,
+PRIMARY KEY (Salary_id),
+FOREIGN KEY (Employee_Id) REFERENCES Employee(Employee_Id)
+);
+
+
+select * from Salary
+
+
+insert into Salary values
+(1,15800.00, 100.00, 400.00, 300.00,15500.00),
+(2,11240.00, 400.00, 500.00, 300.00,14500.00),
+(3,4800.00, 400.00, 300.00, 300.00,13500.00);
+
+
+
+
+--UC12 Ensure all retrieve queries done on UC4, UC5 and UC7
+
+/*UC4 Retrieve all records for employee*/
+select emp.Employee_Id, emp.Emp_Name, emp.Start_Date, emp.Gender, emp.Emp_Address, emp.Phone_Number,
+dept.Dept_no, dept.Dept_name,dept.Dept_location,
+sal.Salary_id, sal.Basic_pay, sal.Deductions, sal.Taxable_pay, sal.Income_tax, sal.Net_pay
+from Employee emp, Department dept, Salary sal
+where emp.Dept_no = dept.Dept_no and  sal.Employee_Id = emp.Employee_Id;
+
+/* UC5 Retrieving all the records from the Salary table for Bill*/
+select emp.Employee_Id, emp.Emp_Name, emp.Start_Date, emp.Gender, emp.Emp_Name, emp.Phone_Number,
+sal.Salary_id, sal.Basic_pay, sal.Deductions, sal.Taxable_pay, sal.Income_tax, sal.Net_pay
+from Employee emp, Salary sal
+where emp.Employee_Id = sal.Employee_Id and emp.Emp_Name = 'Bill';
+
+--UC6 Retrieving all the records 
+select * from Employee emp, Department dept, Salary sal
+where emp.Dept_no = dept.Dept_no and sal.Employee_Id = emp.Employee_Id and 
+ emp.start_date between CAST('05-12-2018' as date) and SYSDATETIME()
+
+/*UC7--> aggregate functions */
+SELECT Sum(Net_pay) as Sum_Salary
+FROM Salary INNER JOIN Employee 
+ON Salary.Employee_Id=Employee.Employee_Id
+WHERE Gender='M' GROUP BY Gender;
+
+SELECT Avg(Net_pay) as Avg_Salary
+FROM Salary INNER JOIN Employee 
+ON Salary.Employee_Id=Employee.Employee_Id
+WHERE Gender='M' GROUP BY Gender;
+
+SELECT Min(Net_pay) as Min_Salary
+FROM Salary INNER JOIN Employee 
+ON Salary.Employee_Id=Employee.Employee_Id
+WHERE Gender='M' GROUP BY Gender;
+
+SELECT Max(Net_pay) as Max_Salary
+FROM Salary INNER JOIN Employee 
+ON Salary.Employee_Id=Employee.Employee_Id
+WHERE Gender='M' GROUP BY Gender;
+
